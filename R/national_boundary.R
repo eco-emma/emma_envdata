32113525ff4c9cb430117323aac80afaf58453b1
Adam M. Wilson <adammwilson@users.noreply.github.com>
2026-01-09 10:13:01 -0500

    Update national_boundary function to download GeoJSON directly and remove GDB conversion step

--- R/national_boundary.R
+++ R/national_boundary.R
@@ -11,19 +11,14 @@ national_boundary <- function(){
     options(timeout = 1000)
   }
 
-  url="https://data.humdata.org/dataset/061d4492-56e8-458c-a3fb-e7950991adf0/resource/69c947ad-dc17-416c-bafa-b66fb523c322/download/zaf_admin_boundaries.gdb.zip"
+  url="https://data.humdata.org/dataset/061d4492-56e8-458c-a3fb-e7950991adf0/resource/37175ff4-41a3-4753-a2c3-ced24142a96c/download/zaf_admin_boundaries.geojson.zip"
   tmpfile1=tempfile()
   tmpdir1=tempdir()
   download.file(url,destfile = tmpfile1)
   unzip(tmpfile1,exdir=tmpdir1)
 
-  # Use ogr2ogr to convert GDB to GeoJSON to avoid GDAL GDB driver issues
-  gdb_path <- file.path(tmpdir1, "zaf_admin_boundaries.gdb")
-  geojson_path <- file.path(tmpdir1, "zaf_admin0.geojson")
-  system(paste0("ogr2ogr -f GeoJSON ", geojson_path, " ", gdb_path, " zaf_admin0"))
-  
   # Read the converted GeoJSON, union, and convert to SpatVector
-  country <- st_read(geojson_path, quiet = TRUE) |>
+  country <- st_read(file.path(tmpdir1, "zaf_admin0.geojson"), quiet = TRUE) |>
     st_union() |>
     st_as_sf() |>
     vect()
